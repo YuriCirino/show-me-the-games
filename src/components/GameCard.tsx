3a17e130a1b3d3e1e7c83db8f98896e3cc5c8236
Yuri Cirino <yuricirino@outlook.com>
2023-06-25 20:33:27 -0300

    adding media queries

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,16 +3,18 @@ import type IGame from "../types/IGame"
 export default function GameCard({ game }: { game: IGame }) {
     return (
         <div
-            className=" flex flex-col justify-between  bg-zinc-100 transition-all duration-300 rounded-xl border-violet-200 border-solid border-2 col-span-4
-        hover:-translate-y-2
+            className=" flex flex-col justify-between  bg-zinc-100 transition-all duration-300 rounded-xl border-violet-200 border-solid border-2 
+            lg:col-span-4
+            md:col-span-6
+            max-sm:col-span-12
+            hover:-translate-y-2
         shadow-sm shadow-violet-200 hover:shadow-violet-300"
         
         >
             <div className="w-full">
                 {/* Card Header */}
-                <div>
 
-                </div>
+
                 <img
                     className=" w-full rounded-md rounded-bl-none rounded-br-none border-b-2 border-violet-500"
                     src={game.thumbnail}
@@ -52,7 +54,7 @@ export default function GameCard({ game }: { game: IGame }) {
             </div>
 
             <div className="p-4 pt-0">{/* Card Foooter */}
-            <a href="" ><button className=" flex justify-center items-center gap-2 p-2 rounded-md text-lg text-white bg-violet-400 w-full" ><Handbag size={"1.5rem"} /> Ver na Loja</button></a>
+            <a href={game.game_url} target="_blank" ><button className=" flex justify-center items-center gap-2 p-2 rounded-md text-lg text-white w-full shadow-md shadow-sky-200 bg-gradient-to-r from-sky-500 to-violet-500" ><Handbag size={"1.5rem"} /> Ver na Loja</button></a>
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,18 @@ import Logo from "../assets/logo.svg"
 
 export default function Home() {
     return (
-        <main className="px-20 py-9 grid grid-cols-12 grid-flow-row gap-5 ">
+        <main className=" grid grid-cols-12 grid-flow-row gap-5 
+        lg:px-20 lg:py-9
+        md:px-12 md:py-6
+        max-sm:px-6 max-sm:py-4
+        "
+        
+        >
             <div
-                className="col-start-4 col-end-10 
+                className="
+                lg:col-start-4 lg:col-end-10 
+                md:col-start-2 md:col-end-12 
+                max-sm:col-start-1 max-sm:col-end-13 
             flex justify-center items-center gap-1
              bg-white border-2 
             border-violet-300 rounded-md
@@ -13,10 +22,10 @@ export default function Home() {
             shadow-sm shadow-violet-100
             "
             >
-                <img src={Logo} width={100} className="p-2  "></img>
+                <img src={Logo}className="p-2 w-24 max-sm:w-16 "></img>
                 <div className="h-3/4 border-solid border-violet-300 border-l-2">
                 </div>
-                <h1 className="text-center text-5xl px-1 py-3 font-bold text-violet-500 rounded-br-3xl">
+                <h1 className="text-center text-5xl md:text-4xl max-sm:text-xl px-1 py-3 font-bold text-violet-500 rounded-br-3xl">
                     Show me the Games!
                 </h1>
             </div>
